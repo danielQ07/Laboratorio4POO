3e60c8d04c11cbe6628eed2afe35b3f8be45e474
danielQ07 <56702587+danielQ07@users.noreply.github.com>
2019-12-11 18:30:22 -0600

    cambios

--- PRY_LAB4_Quirós_Daniel_Robles_Kevin/logicadenegocios/ChipPrepago.java
+++ PRY_LAB4_Quirós_Daniel_Robles_Kevin/logicadenegocios/ChipPrepago.java
@@ -11,11 +11,73 @@ public class ChipPrepago{
   private int indiceLlamadas = 0;
   private int indiceMensajes = 0;
   private int cantidadSalvame = 0;
-  private String historialLlamadas;
-  private String historialMensajes;
-  private String historialNavegaciones;
+  private String historialLlamadas; //cambiar por objeto
+  private String historialMensajes; //cambiar por objeto
+  private String historialNavegaciones; //cambiar por objeto
   
   public ChipPrepago(){
-    
+    this.cantidadChipPrepago += 1;
+    this.codigoPais = "506";
+    this.numeroChip = this.codigoPais + String.valueOf(this.cantidadChipPrepago);
+  }
+  public ChipPrepago(String pCodigoPais){
+    this.cantidadChipPrepago += 1;
+    this.codigoPais = pCodigoPais;
+    this.numeroChip = this.codigoPais + String.valueOf(this.cantidadChipPrepago);
+  }
+  
+  public String activar(String pDueno, double pMegaBytes){
+    String mensaje="";
+    if(this.isActivado==true){
+      return (mensaje += "El chip ya ha sido activado");
+    }else{
+      this.dueno = pDueno;
+      this.megaBytes = pMegaBytes;
+      this.saldo = 1000;
+      this.historialLlamadas = "nueva llamada";
+      this.historialMensajes = "nuevo mensaje";
+      this.historialNavegaciones = "nueva navegacion";
+      this.isActivado = true;
+      return (mensaje = "El chip ha sido activado");
+    }
+  }
+  
+  public String consultarSaldo(){
+    return String.valueOf(this.saldo);
+  }
+  
+  public String consultarHistorial(){
+   return "falta meter el tipo de dato y completar método";
+  }
+  
+  public String recargar(int pMonto){
+    String mensaje="";
+    if(pMonto > 0){
+      return(mensaje += "El monto no puede ser negativo");
+    }else if(pMonto == 0){
+      return(mensaje += "El monto no puede ser 0");
+    }else{
+      this.saldo += pMonto;
+      return ("El monto actual del chip es de " + String.valueOf(this.saldo));
+    }
+  }
+  
+  public String salvame(){
+    String mensaje = "";
+    if(this.cantidadSalvame < 3){
+      if(this.saldo > 0){
+        return(mensaje = ("La cuenta aún posee un saldo de " + String.valueOf(this.saldo)));
+      }else{
+        this.saldo += 100;
+        this.cantidadSalvame += 1;
+        return("La cuenta ha sido recargada con 100 colones con éxito");
+      }
+    }else{
+      return(mensaje = "La cuenta ya realizó las peticiones máximas");
+    }
+  }
+  
+  public String llamar(int pMinutos){
+    return "trbajando en esta";
   }
 }
