599efc542e23be60b5373a84f927893fe1530e85
Kevin <krobles48@outlook.com>
2019-12-13 17:07:09 -0600

    casi

--- PRY_LAB4_Quirós_Daniel_Robles_Kevin/aplicacion/AplChipPrepago.java
+++ PRY_LAB4_Quirós_Daniel_Robles_Kevin/aplicacion/AplChipPrepago.java
@@ -12,18 +12,20 @@ public class AplChipPrepago{
   public static void main(String[] args){
       
     ChipPrepago chip1 = new ChipPrepago();
-    ChipPrepago chip2 = new ChipPrepago("1");
+    ChipPrepago chip2 = new ChipPrepago("201");
+    ChipPrepago chip3 = new ChipPrepago();
     
     System.out.println(chip1.activar("Kevin",100.0));
     System.out.println(chip2.activar("Daniel",150.0));
+    System.out.println(chip1.activar("Prueba",0.0));
     System.out.println();
     
     System.out.println(chip1.recargar(1300));
-    System.out.println(chip2.recargar(1600));
     System.out.println();
     
     System.out.println(chip1.llamar(10,chip2));
     System.out.println(chip2.llamar(5,chip1));
+    System.out.println(chip2.llamar(12,chip3));
     System.out.println();
     
     System.out.println(chip1.enviarSms("Hola Daniel",chip2));
@@ -35,28 +37,30 @@ public class AplChipPrepago{
     System.out.println();
     
     System.out.println(chip1.transferir(200,chip2));
-    System.out.println(chip2.transferir(1000,chip1));
+    System.out.println(chip2.transferir(10000,chip1));
     System.out.println();
     
-    System.out.println(chip1.consultarHistorial());
+    System.out.println(chip3.activar("Isaac",25.0));
+    System.out.println(chip3.llamar(10,chip2));
+    System.out.println(chip3.salvame());
+    System.out.println(chip1.salvame());
+    System.out.println();
+    
     System.out.println(chip2.consultarHistorial());
     System.out.println();
+    System.out.println(chip2.consultarLlamadas());
+    System.out.println(chip1.consultarMensajes());
     
     System.out.println(chip1.consultarSaldo());
-    System.out.println(chip2.consultarSaldo());
     System.out.println();
     
     System.out.println(chip1.verActividadesMesActual());
     System.out.println(chip2.verActividadesNumMes(12));
-    System.out.println();
     
     System.out.println(chip1.verMensajesRecibidos());
-    System.out.println();
     
     System.out.println(ChipPrepago.verCantidadChipsPregago());
-    System.out.println();
     
-    System.out.println(chip1.toString());
     System.out.println(chip2.toString());
   }
 }
